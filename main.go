8a2abf8dc01932839a5a972ffccd2cb0a3d66e4c
Ao <shenlan.wa@alibaba-inc.com>
2024-05-07 16:07:28 +0800

    refactor: 优化启动流程并处理等待Ollama服务的逻辑

--- main.go
+++ main.go
@@ -1,132 +1,128 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
 	"os/exec"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
-const ollamaURL = "http://0.0.0.0:11434/api/chat"
+const (
+	ModelfileDir   = "/work/ollama_model/%s_modelfile"
+	Ollama         = "ollama"
+	OllamaEndpoint = "http://0.0.0.0:11434"
 
-var MODEL = "qwen_7b"
+	Initialize = "/initialize"
+	Path       = "path"
+)
 
-//var MODEL = "qwen_0_5b"
-
-type inputPrompt struct {
-	Prompt string `json:"prompt"`
+func initOllama() (*exec.Cmd, error) {
+	cmd := exec.Command(Ollama, "serve")
+	if err := cmd.Start(); err != nil {
+		return nil, err
+	}
+	if err := waitForOllama(10); err != nil {
+		cmd.Process.Kill()
+		return nil, err
+	}
+	return cmd, nil
 }
 
-type prompt struct {
-	Model    string    `json:"model"`
-	Messages []message `json:"messages"`
-	Stream   bool      `json:"stream"`
+// waitForOllama waits for the ollama service to be ready.
+func waitForOllama(maxAttempts int) error {
+	for i := 0; i < maxAttempts; i++ {
+		log.Printf("Attempt %d to check if ollama is alive...\n", i+1)
+		resp, err := http.Get(OllamaEndpoint)
+		if err != nil {
+			time.Sleep(1 * time.Second)
+			continue
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode == http.StatusOK {
+			return nil
+		}
+		time.Sleep(1 * time.Second)
+	}
+	return fmt.Errorf("failed to start ollama")
 }
 
-type message struct {
-	Role    string `json:"role"`
-	Content string `json:"content"`
+func startOllamaService(model string) error {
+	// initialize ollama
+	ollamaCmd, err := initOllama()
+	if err != nil {
+		return err
+	}
+
+	// create model from an existing modelfile
+	log.Printf("Creating model '%s'...\n", model)
+	createArgs := []string{"create", model, "-f", fmt.Sprintf(ModelfileDir, model)}
+	if err = executeCommand(Ollama, createArgs...); err != nil {
+		ollamaCmd.Process.Kill()
+		return err
+	}
+
+	// run model to provide LLM service
+	log.Printf("Running model '%s'...\n", model)
+	runArgs := []string{"run", model}
+	if err = executeCommand(Ollama, runArgs...); err != nil {
+		ollamaCmd.Process.Kill()
+		return err
+	}
+	return nil
+}
+
+func executeCommand(command string, args ...string) error {
+	cmd := exec.Command(command, args...)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Printf("Error executing command: %v\nOutput: %s\n", err, output)
+		return err
+	}
+	return nil
+}
+
+func reverseProxy(c *gin.Context) {
+	if c.Param(Path) == Initialize && c.Request.Method == http.MethodPost {
+		modelName := os.Getenv("MODEL")
+		const defaultModel = "qwen_0_5b"
+		if modelName == "" {
+			modelName = defaultModel
+		}
+		log.Printf("Starting ollama service with %s\n", modelName)
+		err := startOllamaService(modelName)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Failed to start ollama service with %s due to %v", modelName, err)})
+		}
+		c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("Successfully started ollama service with %s", modelName)})
+		return
+	}
+
+	target, err := url.Parse(OllamaEndpoint)
+	if err != nil {
+		panic(err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(target)
+	proxy.Director = func(req *http.Request) {
+		req.Header = c.Request.Header
+		req.Host = target.Host
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+		req.URL.Path = c.Param(Path)
+	}
+	proxy.ServeHTTP(c.Writer, c.Request)
 }
 
 func main() {
-	fmt.Println("start gin server")
 	r := gin.Default()
-
-	r.POST("/initialize", func(c *gin.Context) {
-		startOllamaModel()
-		c.String(200, "start model finished")
-	})
-
-	r.GET("/ping", func(c *gin.Context) {
-		input := inputPrompt{
-			Prompt: "who are you",
-		}
-		err := doChat(input)
-		if err != nil {
-			c.JSON(500, gin.H{"error": err.Error()})
-		}
-		c.JSON(200, "inference finished")
-	})
-
-	fmt.Println("start ollama init")
-	go initOllama()
-	time.Sleep(10 * time.Second)
-	fmt.Println("finish ollama init")
+	r.Any("/*"+Path, reverseProxy)
 
 	r.Run("0.0.0.0:8000")
-}
-
-func initOllama() {
-	// execute cmd
-	cmd := exec.Command("ollama", "serve")
-	// get cmd response and print
-	out, err := cmd.Output()
-	fmt.Println("output get finished")
-	if err != nil {
-		panic(err)
-	}
-	println(string(out))
-}
-
-func startOllamaModel() {
-	// execute cmd and get output and print
-	args := []string{"create", MODEL, "-f", fmt.Sprintf("./ollama_model/%s_modelfile", MODEL)}
-	cmd := exec.Command("ollama", args...)
-	// get cmd response and print
-	out, err := cmd.Output()
-	if err != nil {
-		panic(err)
-	}
-
-	cmd = exec.Command("ollama", "run", MODEL)
-	out, err = cmd.Output()
-	if err != nil {
-		fmt.Println("err is", err)
-		panic(err)
-	}
-
-	println(string(out))
-}
-
-func doChat(input inputPrompt) error {
-	fmt.Println("input: ", input.Prompt)
-
-	ollamaInput := prompt{
-		Model: MODEL,
-		Messages: []message{
-			{
-				Role:    "user",
-				Content: input.Prompt,
-			},
-		},
-		Stream: false,
-	}
-
-	jsonData, err := json.Marshal(ollamaInput)
-	if err != nil {
-		fmt.Printf("Error marshaling JSON: %v\n", err)
-		return err
-	}
-
-	resp, err := http.Post(ollamaURL, "application/json", bytes.NewBuffer(jsonData))
-	if err != nil {
-		fmt.Printf("Error making POST request: %v\n", err)
-		return err
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Printf("Error reading response body: %v\n", err)
-		return err
-	}
-
-	fmt.Printf("Status code: %d\n", resp.StatusCode)
-	fmt.Println("Body:", string(body))
-	return nil
 }
